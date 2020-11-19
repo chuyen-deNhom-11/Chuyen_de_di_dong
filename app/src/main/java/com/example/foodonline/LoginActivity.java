9ab2f44b6c0576acded1246aa2f066a925dbe65f
hoangduy81 <nguyenhoangduy18032000@gmail.com>
2020-11-19 09:51:39 +0700

    layout sign in

--- app/src/main/java/com/example/foodonline/LoginActivity.java
+++ app/src/main/java/com/example/foodonline/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.foodonline.User.HomeUserActivity;
 
@@ -13,17 +14,25 @@ public class LoginActivity extends AppCompatActivity {
 
     Button sign_in;
     Intent intent;
+    TextView sign_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
         sign_in = findViewById(R.id.sign_in);
-
+        sign_up = findViewById(R.id.sign_up);
         sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 intent = new Intent(LoginActivity.this, HomeUserActivity.class);
+                startActivity(intent);
+            }
+        });
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(LoginActivity.this, SignInActivity.class);
                 startActivity(intent);
             }
         });
