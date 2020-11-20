e85709ac8f9bbd2d7f88204ca693a128d1c18361
Huy Bùi <huyza.2911@gmail.com>
2020-11-20 07:02:22 +0700

    add fire base and layout bill

--- app/src/main/java/com/example/foodonline/LoginActivity.java
+++ app/src/main/java/com/example/foodonline/LoginActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.foodonline.User.HomeUserActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,7 +21,11 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
         sign_in = findViewById(R.id.sign_in);
-
+// Write a message to the database
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        myRef.setValue("Hello, World!");
         sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
