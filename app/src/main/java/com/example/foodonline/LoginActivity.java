1eee6756ffa078b24d1590823391f26df44231c1
Huy Bùi <huyza.2911@gmail.com>
2020-12-08 18:37:35 +0700

    edit conflig P2

--- app/src/main/java/com/example/foodonline/LoginActivity.java
+++ app/src/main/java/com/example/foodonline/LoginActivity.java
@@ -1,56 +1,28 @@
 package com.example.foodonline;
 
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-
-import android.view.KeyEvent;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-
-import com.example.foodonline.DataModel.UserModel;
-import com.example.foodonline.User.HomeUserActivity;
-import com.example.foodonline.utils.SharePreferenceUtils;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.foodonline.DataModel.AcountModel;
-import com.example.foodonline.User.HomeUserActivity;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.iid.FirebaseInstanceId;
-
-import java.util.regex.Pattern;
-
-import static com.example.foodonline.utils.Constant.USER_REFERENCES;
-
-import com.google.firebase.database.IgnoreExtraProperties;
-import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -78,11 +50,6 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
-// Write a message to the database
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("message");
-//        myRef.setValue("Hello, World!");
-
         sign_up = findViewById(R.id.sign_up);
         editTextEmail = findViewById(R.id.edt_email);
         editTextPass = findViewById(R.id.edt_pass);
@@ -92,165 +59,13 @@ public class LoginActivity extends AppCompatActivity {
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
 
-        sign_in.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                intent = new Intent(LoginActivity.this, HomeUserActivity.class);
-//                startActivity(intent);
-                if (editTextEmail.getText().toString().trim().isEmpty() || editTextPass.getText().toString().trim().isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
-                } else if (!EMAIL_ADDRESS.matcher(editTextEmail.getText().toString().trim().trim()).matches()) {
-                    Toast.makeText(LoginActivity.this, "Địa chỉ email không đúng", Toast.LENGTH_SHORT).show();
-                } else {
-
-
-                    firebaseAuth.signInWithEmailAndPassword(editTextEmail.getText().toString().trim(), editTextPass.getText().toString().trim())
-                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-
-
-                                        FirebaseDatabase.getInstance().getReference(USER_REFERENCES).child(firebaseAuth.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-                                            @Override
-                                            public void onDataChange(DataSnapshot dataSnapshot) {
-                                                //  String em = String.valueOf(snapshot.getValue());
-                                                if (dataSnapshot.exists()) {
-                                                    // Toast.makeText(LoginActivity.this, "You already registed", Toast.LENGTH_SHORT).show();
-                                                    String uid = firebaseAuth.getCurrentUser().getUid();
-                                                    firebaseDatabase.getReference("tokens").child(uid).child(FirebaseInstanceId.getInstance().getToken()).push().setValue(true);
-
-                                                    final UserModel userModel = dataSnapshot.getValue(UserModel.class);
-                                                    new Handler().postDelayed(new Runnable() {
-                                                        @Override
-                                                        public void run() {
-                                                            Intent intent = new Intent(LoginActivity.this, HomeUserActivity.class);
-                                                            SharePreferenceUtils.saveUserInfor(LoginActivity.this, userModel);
-                                                            startActivity(intent);
-                                                            finish();
-                                                        }
-                                                    }, 1000);
-
-
-                                                    // goToHomeActivity(userModel);
-                                                } else {
-                                                    // showRegisterDialog(user);
-                                                    Toast.makeText(LoginActivity.this, "kkk", Toast.LENGTH_SHORT).show();
-                                                }
-
-
-                                            }
-
-                                            @Override
-                                            public void onCancelled(@NonNull DatabaseError databaseError) {
-                                                // Util.dismissDialog(LoginActivity.this);
-                                                Log.d("TAG", "onCancelled: " + databaseError.getMessage());
-                                            }
-                                        });
-                                    } else {
-                                        //  Util.dismissDialog(LoginActivity.this);
-                                        Toast.makeText(LoginActivity.this, "" + (task).getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-
-                            });
-                }
-            }
-
-
-//        creat();
-//        eventClick();
-        });
     }
 
-//    private void eventClick() {
-//        sign_in.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                userName = et_acount.getText().toString();
-//                passWord = etPassword.getText().toString();
-//                if (!checkAcount(userName, passWord)) {
-//                    Toast.makeText(LoginActivity.this, R.string.wrong_login, Toast.LENGTH_SHORT).show();
-//                } else {
-//                    checkAcount(userName, passWord);
-//
-//                }
-//            }
-//        });
-//
-//        sign_up.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                intent = new Intent(LoginActivity.this, SignInActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        etPassword.setOnKeyListener(new View.OnKeyListener() {
-//            @Override
-//            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-//                if(keyEvent.getAction() == KeyEvent.ACTION_DOWN){
-//                    switch (i){
-//                        case KeyEvent.KEYCODE_DPAD_CENTER:
-//                        case KeyEvent.KEYCODE_ENTER:
-//                            userName = et_acount.getText().toString();
-//                            passWord = etPassword.getText().toString();
-//                            if (!checkAcount(userName, passWord)) {
-//                                Toast.makeText(LoginActivity.this, R.string.wrong_login, Toast.LENGTH_SHORT).show();
-//                            } else {
-//                                checkAcount(userName, passWord);
-//                            }
-//                        return true;
-//                        default:
-//                            break;
-//                    }
-//                }
-//                return false;
-//            }
-//        });
-//    }
-
-    //    TODO: Check acount login
-//    private boolean checkAcount(String userName, String passWord) {
-//        for (int i = 0; i < listAcount.size(); i++) {
-//            if (userName.equals(listAcount.get(i).getTaikhoan()) && passWord.equals(listAcount.get(i).getMatkhau())) {
-//                if (listAcount.get(i).getRole() == 0) {
-//                    intent = new Intent(LoginActivity.this, HomeUserActivity.class);
-//                    intent.putExtra("username", userName);
-//                }
-//                startActivity(intent);
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
     private void creat() {
         sign_in = findViewById(R.id.sign_in);
         sign_up = findViewById(R.id.sign_up);
     }
 
-    //    @IgnoreExtraProperties
-//    public class User {
-//
-//        public String username="a";
-//        public String email="b";
-//
-//        public User() {
-//            // Default constructor required for calls to DataSnapshot.getValue(User.class)
-//        }
-//
-//        public User(String username, String email) {
-//            this.username = username;
-//            this.email = email;
-//        }
-//
-//    }
-//    private void writeNewUser(String userId, String name, String email) {
-//        User user = new User(name, email);
-//        DatabaseReference mDatabase;
-//
-//        mDatabase = FirebaseDatabase.getInstance().getReference();
-//        mDatabase.child("users").push().setValue(user);
-//    }
 
     //    TODO: read data acount
     private void readData() {
--- app/src/main/java/com/example/foodonline/SignInActivity.java
+++ app/src/main/java/com/example/foodonline/SignInActivity.java
@@ -4,35 +4,21 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.foodonline.DataModel.UserModel;
-import com.example.foodonline.User.HomeUserActivity;
-import com.example.foodonline.utils.SharePreferenceUtils;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.regex.Pattern;
-
-
-import static com.example.foodonline.utils.Constant.USER_REFERENCES;
 
 
 
@@ -94,83 +80,6 @@ public class SignInActivity extends AppCompatActivity {
                 builder.setNegativeButton("Không", null);
                 Dialog dialog = builder.create();
                 dialog.show();
-            }
-        });
-
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-//                edtName = findViewById(R.id.txt_Name);
-//                edtTaiKhoan = findViewById(R.id.txt_takhoan);
-//                edtPass = findViewById(R.id.txt_Pass);
-//                edtConfirmPass = findViewById(R.id.txt_ConfirmPass);
-//                edtAddress = findViewById(R.id.txt_adress);
-//                edtEmail = findViewById(R.id.txt_email);
-//                edtPhoneNumber = findViewById(R.id.txt_PhoneNumber);
-                if (edtName.getText().toString().trim().isEmpty() || edtTaiKhoan.getText().toString().trim().isEmpty() ||
-                        edtPass.getText().toString().trim().isEmpty() || edtConfirmPass.getText().toString().trim().isEmpty() || edtAddress.getText().toString().isEmpty()
-                ||   edtEmail.getText().toString().trim().isEmpty() ||   edtPhoneNumber.getText().toString().trim().isEmpty()
-
-                ) {
-                    Toast.makeText(SignInActivity.this, "Vui lòng  nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
-                } else if (!EMAIL_ADDRESS.matcher(edtEmail.getText().toString().trim().trim()).matches()) {
-                    Toast.makeText(SignInActivity.this, "Địa chỉ email không đúng", Toast.LENGTH_SHORT).show();
-                } else {
-                  //  Util.showDialog(RegisterActivity.this);
-                    if (btnRadioAdmin.isChecked()) {
-                        type = "0";
-                    } else {
-                        type = "1";
-                    }
-
-                    firebaseAuth.createUserWithEmailAndPassword(edtEmail.getText().toString().trim(), edtPass.getText().toString().trim())
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-
-                                        // createTableUser(email, password, name, idServer);
-                                        final UserModel userModel = new UserModel();
-                                        // uid,name,email,phone, dateOfBirth,password
-                                        userModel.setUid(firebaseAuth.getCurrentUser().getUid());
-                                        userModel.setName(edtName.getText().toString());
-                                        userModel.setEmail(edtEmail.getText().toString());
-                                        userModel.setPhoneNumber(edtPhoneNumber.getText().toString());
-                                        userModel.setAddress(edtAddress.getText().toString());
-                                        userModel.setPassword(edtPass.getText().toString());
-                                        userModel.setType(type);
-                                        FirebaseDatabase.getInstance().getReference(USER_REFERENCES).child(userModel.getUid()).setValue(userModel)
-                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                    @Override
-                                                    public void onComplete(@NonNull Task<Void> task) {
-                                                        if (task.isSuccessful()) {
-                                                            Toast.makeText(SignInActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
-                                                            String uid = firebaseAuth.getCurrentUser().getUid();
-                                                            firebaseDatabase.getReference("tokens").child(uid).child(FirebaseInstanceId.getInstance().getToken()).push().setValue(true);
-                                                            new Handler().postDelayed(new Runnable() {
-                                                                @Override
-                                                                public void run() {
-                                                                    Intent intent = new Intent(SignInActivity.this, HomeUserActivity.class);
-                                                                    SharePreferenceUtils.saveUserInfor(SignInActivity.this, userModel);
-                                                                    startActivity(intent);
-                                                                    finish();
-                                                                }
-                                                            }, 1000);
-
-                                                        }
-                                                    }
-                                                });
-                                    } else {
-//                                        Util.dismissDialog(RegisterActivity.this);
-                                        Log.d("TAG", "onComplete: " + task);
-                                        Toast.makeText(SignInActivity.this, "" + (task).getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                        // loginFirebase(email, password, name, idServer);
-                                    }
-                                }
-                            });
-
-                }
             }
         });
 
